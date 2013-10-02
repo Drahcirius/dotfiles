2642a933d58e3f22227e37ac3bb4a1b7fe84f2ef
Richard <hardy263@gmail.com>
2013-10-02 11:32:44 -0400

    added bash aliases

---
+++ .bash_aliases
@@ -0,0 +1,1 @@
+function cd() { builtin cd "$@" && ls; }
---
+++ link.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ln -s dotfiles/.vimrc ../.vimrc 
+ln -s dotfiles/.vim  ../.vim 
+ln -s dotfiles/.bashrc ../.bashrc 
+ln -s dotfiles/.bash_aliases ../.bash_aliases
+
